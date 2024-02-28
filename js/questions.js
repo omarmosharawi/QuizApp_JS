567fde3732e7827d5e7f907f36b3e8bec0f1ad4a
omarmosharawi <ocr115081@gmail.com>
2024-02-28 21:48:43 +0200

    QAPP

---
+++ js/questions.js
@@ -0,0 +1,80 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+ },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What does JSON stand for?",
+    answer: "JavaScript Object Notation",
+    options: [
+      "JavaScript Object Notifier",
+      "JavaScripting Object Network",
+      "JavaScript Object Notation",
+      "Java Standard Object Notation"
+    ]
+ },
+    {
+    numb: 7,
+    question: "Who did this program?",
+    answer: "Omar Mohamed Sharawi",
+    options: [
+      "Omar Mohamed Sharawi",
+      "Mohamed Omar Sharawi",
+      "Sharawi Mohamed Omar",
+      "Sharawi Omar Mohamed"
+    ]
+},
+];
